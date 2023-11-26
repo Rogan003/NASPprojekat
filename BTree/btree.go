55b15720cc6041c5e361ad7b9eed2c4d614d69a7
Veselin Roganovic <veselin.roganovic.rogan003@gmail.com>
2023-11-26 23:15:44 +0100

    [WIP] B-Tree structure, intial functions

--- BTree/btree.go
+++ BTree/btree.go
@@ -21,37 +21,66 @@ type BTree struct {
 // Konstruktor za B stablo, kreira B stablo sa zadatim maximalnim brojem dece i praznim korenskim cvorem
 func (btree *BTree) Init(max int) {
 	btree.maxKids = max
-	node := BTreeNode{nil, nil}
+	node := BTreeNode{make([]int, 0, max - 2), nil}
 	btree.Root = &node
 }
 
 // Funkcija pretrage, za zadati element prolazi kroz stablo i trazi gde on treba da bude
-// vraca cvor (koji, kako) i ako je zadati element tu vraca true, a ako nije vraca false
-func (btree *BTree) Find(elem int) (*BTreeNode, bool) {
+// vraca cvor i ako je zadati element tu vraca true, a ako nije vraca cvor pre kog treba dodati i false
+func (btree *BTree) Find(elem int) (*BTreeNode, int, bool) {
 	iterNode := btree.Root
 
-	for iterNode.keys != nil {
+	for true {
 		indexFound := false
 
 		for index, key := range iterNode.keys {
 			if key == elem {
-				return iterNode, true
+				return iterNode, index, true
 			} else if key > elem {
-				iterNode = iterNode.children[index]
-				indexFound = true
-				break
+				if iterNode.children != nil {
+					iterNode = iterNode.children[index]
+					indexFound = true
+					break
+				} else {
+					return iterNode, index, false
+				}
 			}
 		}
 
 		if !indexFound {
-			iterNode = iterNode.children[len(iterNode.children) - 1]
+			if iterNode.children != nil {
+				iterNode = iterNode.children[len(iterNode.children) - 1]
+			} else {
+				return iterNode, len(iterNode.keys), false
+			}
 		}
 	}
 
-	return iterNode, false
+	return iterNode, 0, false
 }
 
 // Funkcija za dodavanje elementa u B stablo
 func (btree *BTree) Add(elem int) {
+	node, index, isThere := btree.Find(elem)
 
+	if isThere {
+		return
+	} else {
+		if len(node.keys) == index {
+			node.keys = append(node.keys, elem)
+		}
+		node.keys = append(node.keys[:index+1], node.keys[index:]...)
+		node.keys[index] = elem
+	}
+}
+
+// Funkcija za brisanje elementa iz B stabla
+func  (btree *BTree) Delete(elem int) {
+	node, index, isThere := btree.Find(elem)
+
+	if isThere {
+		node.keys = append(node.keys[:index], node.keys[index+1:]...)
+	} else {
+		return
+	}
 }
