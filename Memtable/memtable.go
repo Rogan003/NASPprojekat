2e1d8a2dc89a8edc70c429b6856b3db817674b0c
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-01-15 19:34:37 +0100

    [ADD] flushToDisk to Memtable

--- Memtable/memtable.go
+++ Memtable/memtable.go
@@ -1,9 +1,14 @@
 package Memtable
 
 import (
-	"fmt"
-	"NASPprojekat/SkipList"
 	"NASPprojekat/BTree"
+	"NASPprojekat/BloomFilter"
+	"NASPprojekat/SSTable"
+	"NASPprojekat/SkipList"
+
+	"fmt"
+	"os"
+	"strconv"
 )
 
 /*
@@ -15,11 +20,11 @@ import (
 */
 
 type Memtable struct {
-	skiplist	SkipList.SkipList
-	btree	BTree.BTree
-	version	string
-	maxCap	int
-	curCap	int
+	skiplist SkipList.SkipList
+	btree    BTree.BTree
+	version  string
+	maxCap   int
+	curCap   int
 }
 
 // konstruktor za memtable, na osnovu verzije (skip lista ili b stablo) i maksimalnog kapaciteta kreira inicijalno stanje strukture
@@ -44,11 +49,11 @@ func (mt *Memtable) Add(key string, value []byte, timestamp uint64) {
 	if mt.version == "skiplist" {
 		// mt.skiplist.Add(elem)
 		ok := mt.skiplist.Add(key, value, timestamp)
-		if (ok) {
+		if ok {
 			mt.curCap++
 		}
 	} else {
-		/* DODATI: 
+		/* DODATI:
 		ok := mt.btree.Add(key, value, timestamp)
 		if (ok) {
 			mt.curCap++
@@ -70,11 +75,11 @@ func (mt *Memtable) Delete(key string, timestamp uint64) {
 		// logicko brisanje iz skip liste
 		// ok bool -> vraca true ako je obrisan, false ako smo obrisali element koji ne postoji
 		ok := mt.skiplist.Delete(key, timestamp)
-		if (ok) {
+		if ok {
 			mt.curCap--
 		}
 	} else {
-		// DODATI: 
+		// DODATI:
 		/*
 			ok := mt.btree.Add(key, nil, timestamp)
 			if (ok) {
@@ -102,6 +107,27 @@ func (mt *Memtable) Get(key string) {
 			fmt.Printf("%s %d\n", elem.Key, elem.Value)
 		} else {
 			fmt.Printf("Element sa kljucem %s ne postoji!\n", key)
+		}
+	}
+}
+
+// funkcija prima kljuc i po njemu trazi podatak u memtable, vraca string vrednosti podatka i bool koji oznacava da li je nadjen element
+func (mt *Memtable) GetElement(key string) ([]byte, bool) {
+	if mt.version == "skiplist" {
+		// pronalazak u skip listi
+		skipNode, found := mt.skiplist.Find(key)
+		if skipNode.Elem.Value != nil && !skipNode.Elem.Tombstone && found {
+			return skipNode.Elem.Value, true
+		} else {
+			return []byte{}, false
+		}
+
+	} else {
+		_, _, _, elem := mt.btree.Find(key)
+		if elem != nil && !elem.Tombstone {
+			return elem.Value, true
+		} else {
+			return []byte{}, false
 		}
 	}
 }
@@ -136,4 +162,72 @@ func (mt *Memtable) flush() {
 	}
 
 	mt.curCap = 0
+}
+
+func (m *Memtable) flushToDisk() {
+	fmt.Println("Zapisano na disk.")
+	//citamo podatke prvog nivoa jer u njega flushujemo, osmi sstable na prvom nivou je u fajlu npr SSTable/files/dataFile_1_8
+	var DataFileName = "SSTable/files/dataFile_1"
+	var IndexFileName = "SSTable/files/indexFile_1"
+	var SummaryFileName = "SSTable/files/summaryFile_1"
+	var BloomFilterFileName = "SSTable/files/bloomFilterFile_1"
+	var MerkleTreeFileName = "SSTable/files/merkleTreeFile_1"
+
+	//pravimo bloomfilter za sstable od memtable broja elemenata
+	bf := BloomFilter.BloomFilter{}
+	// da li je bitno da li prosledjujemo m.curCap ili m.maxCap jer se svakako flush zove akd su oni jendkai tj memtable pun??
+	bf.Init(m.maxCap, 0.01)
+
+	//MEMTABLE TREBA DA SE SORTIRA
+
+	// level[] cuva koliko sstableova se nalazi na svakom od nivoa, dodajemo jos jedan sstable
+	var i = level[0] + 1
+	//pravimo fajlove za novi sstable
+	DataFileName += "_" + strconv.Itoa(i) + ".txt"
+	IndexFileName += "_" + strconv.Itoa(i) + ".txt"
+	SummaryFileName += "_" + strconv.Itoa(i) + ".txt"
+	BloomFilterFileName += "_" + strconv.Itoa(i) + ".txt"
+	MerkleTreeFileName += "_" + strconv.Itoa(i) + ".txt"
+
+	//pravljenje fajlova za novi sstable
+	dataFile, _ := os.Create(DataFileName)
+	err := dataFile.Close()
+	if err != nil {
+		return
+	}
+
+	indexFile, _ := os.Create(IndexFileName)
+	err = indexFile.Close()
+	if err != nil {
+		return
+	}
+
+	summaryFile, _ := os.Create(SummaryFileName)
+	err = summaryFile.Close()
+	if err != nil {
+		return
+	}
+
+	bloomFilterFile, _ := os.Create(BloomFilterFileName)
+	err = bloomFilterFile.Close()
+	if err != nil {
+		return
+	}
+
+	merkleTreeFile, _ := os.Create(MerkleTreeFileName)
+	err = merkleTreeFile.Close()
+	if err != nil {
+		return
+	}
+
+	//novi fajlovi se dodaju u liste sa imenima svih fajlova koji cine lsm tree
+	dataFilesNames = append(dataFilesNames, DataFileName)
+	indexFilesNames = append(indexFilesNames, IndexFileName)
+	summaryFilesNames = append(summaryFilesNames, SummaryFileName)
+	bloomFilterFilesNames = append(bloomFilterFilesNames, BloomFilterFileName)
+	merkleTreeFilesNames = append(merkleTreeFilesNames, MerkleTreeFileName)
+
+	//pravimo sstable, mora da se pre prosledjivanja SORTIRA MEMTABLE
+	SSTable.MakeData(m, bf, DataFileName, IndexFileName, SummaryFileName, BloomFilterFileName)
+
 }
