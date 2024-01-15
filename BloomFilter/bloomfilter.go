7f39769a5195b9033395a628a68c247a66255ef3
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-01-15 19:46:16 +0100

    [ADD] return err for Deserialize BloomFilter

--- BloomFilter/bloomfilter.go
+++ BloomFilter/bloomfilter.go
@@ -13,8 +13,8 @@ Seeds - seeds za hash funkcije
 */
 
 type BloomFilter struct {
-	M	[]byte
-	K	uint
+	M     []byte
+	K     uint
 	Seeds []HashWithSeed
 }
 
@@ -28,13 +28,15 @@ func (bf *BloomFilter) Init(expectedElements int, falsePositiveRate float64) {
 }
 
 // proverava da li je dati niz bajtova (element) u bloom filteru
+// check_elem treba da se vrsi samo po kljucu a ne po celom elementu jer nemamo jos element tokom trazenja?
+// PROMENITI
 func (bf BloomFilter) Check_elem(elem []byte) bool {
 	isThere := true
 
 	for _, seed := range bf.Seeds {
 		index := (seed.Hash(elem) % uint64(len(bf.M)))
 
-		if(bf.M[index] == 0) {
+		if bf.M[index] == 0 {
 			isThere = false
 			break
 		}
@@ -48,7 +50,7 @@ func (bf *BloomFilter) Add(elem []byte) {
 	for _, seed := range bf.Seeds {
 		index := (seed.Hash(elem) % uint64(len(bf.M)))
 
-		if(bf.M[index] == 0) {
+		if bf.M[index] == 0 {
 			bf.M[index] = 1
 		}
 	}
@@ -56,8 +58,8 @@ func (bf *BloomFilter) Add(elem []byte) {
 
 // serializacija bloom filtera
 func (bf BloomFilter) Serialize(path string) {
-	file, err := os.OpenFile(path, os.O_RDWR | os.O_CREATE, 0666)
-	if(err != nil) {
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
@@ -65,25 +67,28 @@ func (bf BloomFilter) Serialize(path string) {
 	encoder := gob.NewEncoder(file)
 	err = encoder.Encode(bf)
 
-	if(err != nil) {
+	if err != nil {
 		panic(err)
 	}
 }
 
 // deserializacija bloom filtera
-func (bf *BloomFilter) Deserialize(path string) {
-	file, err := os.OpenFile(path, os.O_RDWR | os.O_CREATE, 0666)
-	if(err != nil) {
-		panic(err)
+func (bf *BloomFilter) Deserialize(path string) error {
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		//panic(err)
+		return err
 	}
 	defer file.Close()
-	
+
 	decoder := gob.NewDecoder(file)
 	file.Seek(0, 0)
 	for {
 		err = decoder.Decode(bf)
 		if err != nil {
-			break
+			return err
 		}
 	}
+	//zasto je unreachable???
+	return nil
 }
