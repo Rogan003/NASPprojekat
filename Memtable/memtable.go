59a21f7080191003508bcaf69ee7c4443385bffc
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-01-29 15:18:44 +0100

    [UPDATE] files for flush

--- Memtable/memtable.go
+++ Memtable/memtable.go
@@ -57,10 +57,10 @@ slobodnu, a ako jesu ipak sve popunjenje, flushujemo poslednju read-only tabelu 
 oba pokazivaca l' i 'r' za jedno mjesto ispred.
 */
 type NMemtables struct {
-	N   int         // broj memtabli
-	Arr []*Memtable // niz memtabli
-	l   int         // left index
-	R   int         // right index
+	N   int             // broj memtabli
+	Arr []*Memtable     // niz memtabli
+	l   int             // left index
+	R   int             // right index
 	lsm *Config.LSMTree // lsm tree from config
 }
 
@@ -81,14 +81,12 @@ func (nmt *NMemtables) Init(vers string, mCap int, num int, lsm *Config.LSMTree)
 	nmt.lsm = lsm
 }
 
-
-
 // funkcija i za dodavanje i za izmenu elementa sa kljucem
 // u zavisnosti od verzije i prisutnosti elementa dodaje elem ili ga menja u odredjenoj strukturi
 // poziva se iz WAL-a, ako je uspesno odradjeno dodavanje/izmena
 func (nmt *NMemtables) Add(key string, value []byte) int {
 
-	arr := nmt.Arr         // arr memtabli
+	arr := nmt.Arr // arr memtabli
 	ind := nmt.R
 	memtable := arr[nmt.R] // prva "aktivna" memtabla
 
@@ -105,8 +103,8 @@ func (nmt *NMemtables) Add(key string, value []byte) int {
 
 	if memtable.curCap == memtable.maxCap {
 		if (nmt.R-nmt.l == nmt.N-1) || (nmt.R < nmt.l) {
-			memtableLast := arr[nmt.l] // izbrisala sam proveru da li je memtable empty
-			memtableLast.flush(nmt.lsm)       // valjda nece trebati (testiracu)
+			memtableLast := arr[nmt.l]  // izbrisala sam proveru da li je memtable empty
+			memtableLast.flush(nmt.lsm) // valjda nece trebati (testiracu)
 			nmt.l = (nmt.l + 1) % nmt.N
 		}
 		nmt.R = (nmt.R + 1) % nmt.N
@@ -114,7 +112,6 @@ func (nmt *NMemtables) Add(key string, value []byte) int {
 
 	return ind
 }
-
 
 // pomocna funckija za Delete(),
 // kada se brise element koji je u starijim memtabelama
@@ -140,16 +137,13 @@ func (nmt *NMemtables) AddAndDelete(key string, value []byte) {
 
 	if memtable.curCap == memtable.maxCap {
 		if (nmt.R-nmt.l == nmt.N-1) || (nmt.R < nmt.l) {
-			memtableLast := arr[nmt.l] // izbrisala sam proveru da li je memtable empty
-			memtableLast.flush(nmt.lsm)       // valjda nece trebati (testiracu)
+			memtableLast := arr[nmt.l]  // izbrisala sam proveru da li je memtable empty
+			memtableLast.flush(nmt.lsm) // valjda nece trebati (testiracu)
 			nmt.l = (nmt.l + 1) % nmt.N
 		}
 		nmt.R = (nmt.R + 1) % nmt.N
 	}
 }
-
-
-
 
 // funkcija za brisanje elementa sa kljucem iz memtable
 // brisanje je logicko
@@ -158,16 +152,16 @@ func (nmt *NMemtables) Delete(key string) (bool, int) {
 
 	data, found, primaryMemtable := nmt.Get(key)
 
-	if (found) {
+	if found {
 		arr := nmt.Arr
 		ind := nmt.R
 		memtable := arr[nmt.R]
-	
+
 		if memtable.version == "skiplist" {
 			// logicko brisanje iz skip liste
 			// funkcija za brisanje -> vraca true ako je obrisan, false ako smo obrisali element koji ne postoji
-	
-			if (primaryMemtable) {
+
+			if primaryMemtable {
 				// ako se nalazi u aktivnoj tabeli, samo obrisi
 				return memtable.skiplist.Delete(key), ind
 			} else {
@@ -177,8 +171,8 @@ func (nmt *NMemtables) Delete(key string) (bool, int) {
 				return true, ind
 			}
 		} else {
-			if (primaryMemtable) {
-				return memtable.btree.Add(key, nil), ind
+			if primaryMemtable {
+				return memtable.btree.Add(key, nil)
 			} else {
 				// ne nalazi se u aktivnoj, nego u nekoj od proslih read-only tabela
 				// dodaj u primarni memtable pa onda izbrisi
@@ -190,24 +184,22 @@ func (nmt *NMemtables) Delete(key string) (bool, int) {
 		//fmt.Printf("Element sa kljucem %s ne postoji!\n", key)
 		return false, -1
 	}
-	
-	
+
 }
-
-
 
 // funkcija prima kljuc i po njemu trazi podatak u memtable,
 // VRACA: string vrijednosti podatka, bool koji oznacava da li je nadjen element
-//        i bool ("primary") koji oznacava da li se element nalazi u prvoj memtabeli ili u nekoj starijoj
-//        -> (ovaj poslednji bool je potreban zbog Delete)
+//
+//	i bool ("primary") koji oznacava da li se element nalazi u prvoj memtabeli ili u nekoj starijoj
+//	-> (ovaj poslednji bool je potreban zbog Delete)
 func (nmt *NMemtables) Get(key string) ([]byte, bool, bool) {
 
 	arr := nmt.Arr
-	r := nmt.R   // pretragu pocinjemo od prve aktivne, pa prelazimo dalje na starije
+	r := nmt.R // pretragu pocinjemo od prve aktivne, pa prelazimo dalje na starije
 
-	for (true) {
+	for true {
 		memtable := arr[r]
-		if (memtable.empty && r != nmt.R) {  // ako je naredna memtabela prazna, ni sledece nisu popunjene, nema potrebe dalje da gledamo 
+		if memtable.empty && r != nmt.R { // ako je naredna memtabela prazna, ni sledece nisu popunjene, nema potrebe dalje da gledamo
 			//fmt.Printf("Element sa kljucem %s ne postoji!\n", key)
 			return []byte{}, false, false
 		}
@@ -216,36 +208,36 @@ func (nmt *NMemtables) Get(key string) ([]byte, bool, bool) {
 			skipNode, found := memtable.skiplist.Find(key)
 			if skipNode.Elem.Transaction.Value != nil && found {
 				//fmt.Printf("%s %d\n", skipNode.Elem.Transaction.Key, skipNode.Elem.Transaction.Value)
-				if (skipNode.Elem.Tombstone) {
+				if skipNode.Elem.Tombstone {
 					return []byte{}, false, false
 				}
 
-				if (r == nmt.R) {
-					return skipNode.Elem.Transaction.Value, true, true    
-																						  
+				if r == nmt.R {
+					return skipNode.Elem.Transaction.Value, true, true
+
 				} else {
 					return skipNode.Elem.Transaction.Value, true, false
 				}
-				
+
 			}
 		} else {
 			_, _, _, elem := memtable.btree.Find(key)
 			if elem != nil {
 				//fmt.Printf("%s %d\n", elem.Transaction.Key, elem.Transaction.Value)
-				if (elem.Tombstone) {
+				if elem.Tombstone {
 					return []byte{}, false, false
 				}
 
-				if (r == nmt.R) {
+				if r == nmt.R {
 					return elem.Transaction.Value, true, true
 				} else {
 					return elem.Transaction.Value, true, false
 				}
-			} 
+			}
 		}
 
 		r = (r - 1 + nmt.N) % nmt.N
-		if (r == nmt.R) {  // vratili smo se na memtabelu od koje smo krenuli pretragu - prekidamo, nismo nasli podatak
+		if r == nmt.R { // vratili smo se na memtabelu od koje smo krenuli pretragu - prekidamo, nismo nasli podatak
 			break
 		}
 	}
@@ -254,14 +246,13 @@ func (nmt *NMemtables) Get(key string) ([]byte, bool, bool) {
 	return []byte{}, false, false
 }
 
-
 // funkcija koja radi flush na disk (sstable)
 func (mt *Memtable) flush(lsm *Config.LSMTree) {
 	/*
-	for _, value := range mt.GetSortedElems() {
-		fmt.Printf("%s %d %t %s\n", value.Transaction.Key, value.Transaction.Value, value.Tombstone, value.ToByte())
-	}
-	fmt.Printf("\n")
+		for _, value := range mt.GetSortedElems() {
+			fmt.Printf("%s %d %t %s\n", value.Transaction.Key, value.Transaction.Value, value.Tombstone, value.ToByte())
+		}
+		fmt.Printf("\n")
 	*/
 
 	mt.flushToDisk(lsm)
@@ -278,7 +269,7 @@ func (mt *Memtable) flush(lsm *Config.LSMTree) {
 	mt.curCap = 0
 }
 
-func (mt *Memtable) GetSortedElems() ([]*Config.Entry) {
+func (mt *Memtable) GetSortedElems() []*Config.Entry {
 
 	// arr := nmt.Arr
 	// memtable := arr[nmt.R]
@@ -293,22 +284,22 @@ func (mt *Memtable) GetSortedElems() ([]*Config.Entry) {
 func (m *Memtable) flushToDisk(lsm *Config.LSMTree) {
 	fmt.Println("Zapisano na disk.")
 	//citamo podatke prvog nivoa jer u njega flushujemo, osmi sstable na prvom nivou je u fajlu npr SSTable/files/dataFile_1_8
-	var DataFileName = "SSTable/files/dataFile_1"
-	var IndexFileName = "SSTable/files/indexFile_1"
-	var SummaryFileName = "SSTable/files/summaryFile_1"
-	var BloomFilterFileName = "SSTable/files/bloomFilterFile_1"
-	var MerkleTreeFileName = "SSTable/files/merkleTreeFile_1"
+	var DataFileName = "files_SSTable/dataFile_1"
+	var IndexFileName = "files_SSTable/indexFile_1"
+	var SummaryFileName = "files_SSTable/summaryFile_1"
+	var BloomFilterFileName = "files_SSTable/bloomFilterFile_1"
+	var MerkleTreeFileName = "files_SSTable/merkleTreeFile_1"
 
 	//MEMTABLE TREBA DA SE SORTIRA
 
 	// level[] cuva koliko sstableova se nalazi na svakom od nivoa, dodajemo jos jedan sstable
 	var i = lsm.Levels[0] + 1
 	//pravimo fajlove za novi sstable
-	DataFileName += "_" + strconv.Itoa(i) + ".txt"
-	IndexFileName += "_" + strconv.Itoa(i) + ".txt"
-	SummaryFileName += "_" + strconv.Itoa(i) + ".txt"
-	BloomFilterFileName += "_" + strconv.Itoa(i) + ".txt"
-	MerkleTreeFileName += "_" + strconv.Itoa(i) + ".txt"
+	DataFileName += "_" + strconv.Itoa(i) + ".gob"
+	IndexFileName += "_" + strconv.Itoa(i) + ".gob"
+	SummaryFileName += "_" + strconv.Itoa(i) + ".gob"
+	BloomFilterFileName += "_" + strconv.Itoa(i) + ".gob"
+	MerkleTreeFileName += "_" + strconv.Itoa(i) + ".gob"
 
 	//pravljenje fajlova za novi sstable
 	dataFile, _ := os.Create(DataFileName)
