334de04b76157d88840a561fa1ad778c1aa279dc
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-01-29 16:22:45 +0100

    [UPDATE] WAL model

--- WriteAheadLog/model.go
+++ WriteAheadLog/model.go
@@ -1,53 +1,24 @@
 package WriteAheadLog
 
 import (
-	"NASPprojekat/Config"
+	"os"
 	"time"
 )
 
 type WAL struct {
 	path         string        //putanja do fajla sa walom
-	lastSegment  Segment       //aktivni segment
+	lastSegment  *os.File      //pokazivac na aktivni segment
 	duration     time.Duration //na koji period ce se zvati brisanje
 	lowWaterMark int           //do kog indeksa se brisu segmenti
 	lastIndex    int64         //indeks poslednjeg segmenta u walu
 	segmentSize  int64         //maksimalni broj bajtova u segmentu
-}
+	CurrentSize  int64         //broj popunjenih bajtova u segmentu
 
-type Segment struct { //segment
-	fileName string          //putanja do fajla segmenta
-	index    int64           //pocetak segnemta
-	size     int64           //broj popunjenih bajtova u segmentu
-	entries  []*Config.Entry //entriji u segmentu
-}
-
-// Funkcije za segmente
-// getteri
-func (s *Segment) FileName() string {
-	return s.fileName
-}
-
-func (s *Segment) Entries() []*Config.Entry {
-	return s.entries
-}
-
-func (s *Segment) Size() int64 {
-	return s.size
-}
-
-func (s *Segment) Index() int64 {
-	return s.index
-}
-
-// funkcionalnosti
-func (s *Segment) AppendEntry(entry *Config.Entry) { //upis novog podatka u segment
-	s.entries = append(s.entries, entry)
-	s.size = s.size + 1
 }
 
 // Funkcije za WAL
 // getteri
-func (wal *WAL) LastSegment() Segment {
+func (wal *WAL) LastSegment() *os.File {
 	return wal.lastSegment
 }
 
@@ -71,20 +42,12 @@ func (wal *WAL) SegmentSize() int64 {
 }
 
 // konstruktori
-func NewWAL(path string, duration time.Duration, lowWaterMark int) (*WAL, error) {
+func NewWAL(path string, lastSeg *os.File, duration time.Duration, lowWaterMark int) (*WAL, error) {
 	return &WAL{
 		path:         path,
-		lastSegment:  Segment{},
+		lastSegment:  lastSeg,
 		duration:     duration,
 		lowWaterMark: lowWaterMark,
 		lastIndex:    0,
 	}, nil
-}
-func NewSegment(fileName string, index int64, size int64, entries []*Config.Entry) *Segment {
-	return &Segment{
-		fileName: fileName,
-		index:    index,
-		size:     size,
-		entries:  entries,
-	}
 }
